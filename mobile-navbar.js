4da1f28244c41b64b82cd443e897ea40dbc16056
mariahgc <maria.17aac16@gmail.com>
2025-04-16 06:27:36 -0300

    first commit

---
+++ mobile-navbar.js
@@ -0,0 +1,20 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const navList = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-list li');
+
+mobileMenu.addEventListener('click', () => {
+  navList.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    }
+  });
+});
+
+
+
+
