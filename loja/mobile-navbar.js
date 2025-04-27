0fbe14c3ac700fc2b2bb1247dcda28bc53ac4d58
gustavomurar <gustavomurarosoares3@gmail.com>
2025-04-27 01:20:14 -0300

    Página das emoções criada

---
+++ loja/mobile-navbar.js
@@ -0,0 +1,55 @@
+const mobileMenu = document.querySelector('.mobile-menu');
+const navList = document.querySelector('.nav-list');
+const navLinks = document.querySelectorAll('.nav-list li');
+
+mobileMenu.addEventListener('click', () => {
+  navList.classList.toggle('active');
+  mobileMenu.classList.toggle('active');
+
+  navLinks.forEach((link, index) => {
+    if (link.style.animation) {
+      link.style.animation = '';
+    } else {
+      link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+    }
+  });
+});
+
+
+
+
+
+let items = document.querySelectorAll('.slider .item');
+    let next = document.getElementById('next');
+    let prev = document.getElementById('prev');
+    
+    let active = 3;
+    function loadShow(){
+        let stt = 0;
+        items[active].style.transform = `none`;
+        items[active].style.zIndex = 1;
+        items[active].style.filter = 'none';
+        items[active].style.opacity = 1;
+        for(var i = active + 1; i < items.length; i++){
+            stt++;
+            items[i].style.transform = `translateX(${320*stt}px)`;
+            items[i].style.zIndex = -stt;
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+        stt = 0;
+        for(var i = active - 1; i >= 0; i--){
+            stt++;
+            items[i].style.transform = `translateX(${-320*stt}px) `;
+            items[i].style.zIndex = -stt;
+            items[i].style.opacity = stt > 2 ? 0 : 0.6;
+        }
+    }
+    loadShow();
+    next.onclick = function(){
+        active = active + 1 < items.length ? active + 1 : active;
+        loadShow();
+    }
+    prev.onclick = function(){
+        active = active - 1 >= 0 ? active - 1 : active;
+        loadShow();
+    }
